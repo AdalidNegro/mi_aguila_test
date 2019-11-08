f3d30565af37f9ce59f9a444ae543dbd7f0ead96
Said Fayad <said@grability.com>
2019-11-08 14:28:23 -0500

    DevOps - clustering target groups

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -10,8 +10,9 @@ resource "aws_lb" "mi_aguila_load_balancer" {
 }
 
 resource "aws_lb_target_group" "mi_aguila_target_group" {
-  name        = "${var.aws_region}-${var.environment}-target-group"
-  port        = 3000
+  count = length(var.ports_list)
+  name        = "${var.aws_region}-${element(var.ec2_list, count.index)}-target-group"
+  port        = var.ports_list[count.index]
   protocol    = "TCP"
   vpc_id      = aws_vpc.mi_aguila.id
   target_type = "ip"
@@ -25,12 +26,13 @@ resource "aws_lb_target_group" "mi_aguila_target_group" {
 }
 
 resource "aws_lb_listener" "mi_aguila_listener" {
+  count = length(var.ports_list)
   load_balancer_arn = aws_lb.mi_aguila_load_balancer.id
-  port              = 3000
+  port              = var.ports_list[count.index]
   protocol          = "TCP"
 
   default_action {
-    target_group_arn = aws_lb_target_group.mi_aguila_target_group.id
+    target_group_arn = aws_lb_target_group.mi_aguila_target_group[count.index].id
     type             = "forward"
   }
 }
